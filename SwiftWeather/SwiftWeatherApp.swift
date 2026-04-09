5ef9f2be9d83afe9490ba65500773236ab0825c1
Luna Parker <lunaparker33@gmail.com>
2026-04-09 00:01:49 -0400

    Initial Commit

---
+++ SwiftWeather/SwiftWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWeatherApp.swift
+//  SwiftWeather
+//
+//  Created by Luna Parker on 4/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
